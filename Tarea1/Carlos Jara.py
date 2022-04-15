228d706ae0c669bb22e9a99e6757ef741fe58b56
windu2 <carlos.jaraa@sansano.usm.cl>
2022-04-15 15:22:10 -0400

    logs done

--- Tarea1/Carlos Jara.py
+++ Tarea1/Carlos Jara.py
@@ -15,29 +15,31 @@ secuen_1 = input("Ingrese la 1era secuencia de caracteres a comparar: ")
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 #print(current_time," secuencia 1 ingresada")
-f.write(current_time + " secuencia 1 ingresada"+"\n")
+f.write(current_time + " Secuencia 1 ingresada: "+ secuen_1  +"\n")
 
 
 secuen_2 = input("Ingrese la 2da secuencia de caracteres a comparar: ")
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
-f.write(current_time + " secuencia 2 ingresada"+"\n")
+f.write(current_time + " Secuencia 2 ingresada: "+ secuen_2  +"\n")
 
 
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
-f.write(current_time + " comparando"+"\n")
+f.write(current_time + " Comparando"+"\n")
 
 if(secuen_2 != secuen_1):
 	print("Ambas secuencias no son iguales")
+	f.write("Resultado: Ambas secuencias no son iguales")
 else:
 	print("Ambas secuencias son iguales")
+	f.write("Resultado: Ambas secuencias son iguales")
 
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
-f.write(current_time + " fin de ejecucion" + "\n"+"\n")
+f.write(current_time + " Fin de ejecucion" + "\n"+"\n")
 
 
 #cierre de documento
